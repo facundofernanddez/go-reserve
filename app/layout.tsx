3819f1a28ef86c06b64c266e1168d72ad423dba7
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2025-12-08 18:36:40 -0300

    create reservation form and install dependencies

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import NavBar from "./(components)/Navbar";
+import AppProviders from "./providers";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,10 +29,12 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} bg-white text-black h-full`}
       >
-        <main className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
-          <NavBar />
-          {children}
-        </main>
+        <AppProviders>
+          <main className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
+            <NavBar />
+            {children}
+          </main>
+        </AppProviders>
       </body>
     </html>
   );
