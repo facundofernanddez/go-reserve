e4d3d0400ffb6249cdc474db24d5513c83ac4091
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2025-12-11 08:08:42 -0300

    start to create dashboard route

---
+++ app/api/dashboard/route.ts
@@ -0,0 +1,7 @@
+/**
+ * En esta ruta se manejan las solicitudes GET para obtener los datos del dashboard.
+ * El admin (dueño del complejo) puede ver las canchas que tiene reservadas en el dia por hora y elegir el dia que desea ver.
+ * Si tiene mas de un complejo, puede elegir el complejo del que quiere ver el dashboard.
+ */
+
+export async function GET(request: Request) {}
